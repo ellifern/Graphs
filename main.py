68eb81c5988b6a14101dceaa19ddc9478b4e5d31
ellifern <efernandes111@srrcc.org.uk>
2024-09-25 09:36:13 +0100

    exercise 1

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-# This is a sample Python script.
+adj_list = {
+    'A': ['C', 'B'],
+    'B': ['A', 'D', 'K'],
+    'C': ['A', 'D', 'F'],
+    'D': ['C', 'F', 'G', 'H', 'E'],
+    'E': ['D', 'H', 'K'],
+    'F': ['C', 'G', 'J', 'D'],
+    'G': ['D', 'F', 'H', 'J'],
+    'H': ['E', 'D', 'G'],
+    'K': ['B', 'E', 'D'],
+    'J': ['G', 'F']
+}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def check_neigh(adj_list, node):
+    print(adj_list[node])
+
+def connected(adj_list, node1, node2):
+    if node1 in adj_list[node2]:
+        print (True)
+    else:
+        return (False)
+
+check_neigh(adj_list, 'A')
